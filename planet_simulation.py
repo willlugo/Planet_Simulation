b3946b07d4a5b1cdfcdad0e984b807e15289b099
William Lugo <100079752+willlugo@users.noreply.github.com>
2022-03-03 13:50:18 +0000

    attempted to fix simulation, class conflict error is gone, now planets fly off out of orbit

--- planet_simulation.py
+++ planet_simulation.py
@@ -9,7 +9,8 @@ class Planet():
     TIMESTEP = float(3600 * 24) # 1 day
 
     def __init__(self, x, y, radius, colour, mass):
-        self.pos = np.array([x, y])
+        self.x = x
+        self.y = y
         self.radius = radius # radius planet will be drawn in
         self.colour = colour
         self.mass = mass # mass of planet in kg
@@ -18,39 +19,46 @@ class Planet():
         self.sun = False
         self.distance_to_sun = 0
 
-        self.vel = np.array([float(0), float(0)])
+        self.velx = 0
+        self.vely = 0
     
     def draw(self, win):
-        x = self.pos[0] * self.SCALE + (WIDTH / 2)
-        y = self.pos[1] * self.SCALE + (HEIGHT / 2)
+        x = self.x * self.SCALE + (WIDTH / 2)
+        y = self.y * self.SCALE + (HEIGHT / 2)
         pygame.draw.circle(win, self.colour, (x, y), self.radius)
     
     def attraction(self, other):
-        other_pos = other.pos
-        distance_xy = other_pos - self.pos
-        distance = np.sqrt(distance_xy[0]**2 + distance_xy[1]**2)
-        #distance_x = other_pos[0] - self.pos[0]
-        #distance_y = other_pos[1] - self.pos[1]
-        #distance = np.sqrt(distance_x**2 + distance_y**2)
-        print(f"distance between {self} and {other} is {distance}")
+        other_x, other_y = other.x, other.y
+        distance_x = other_x - self.x
+        distance_y = other_y - other.y
+        distance = np.sqrt(distance_x**2 + distance_y**2)
 
         if other.sun:
             self.distance_to_sun = distance
         
-        force = (self.G * self.mass * other.mass) / (distance_xy**2)
-        return force
+        force = (self.G * self.mass * other.mass) / (distance**2)
+        theta = np.arctan2(distance_y, distance_x)
+        force_x = np.cos(theta) * force
+        force_y = np.sin(theta) * force
+        #force = (self.G * self.mass * other.mass) / (distance_xy**2)
+        return force_x, force_y
     
     def update_position(self, planets):
-        total_f = float(0)
+        total_fx = total_fy = 0
         for planet in planets:
             if self == planet:
                 continue
 
-            total_f += self.attraction(planet)
+            force_x, force_y = self.attraction(planet)
+            total_fx += force_x
+            total_fy += force_y
+
         
-        self.vel += total_f / self.mass * self.TIMESTEP
-        self.pos += self.vel * self.TIMESTEP
-        self.orbit.append((self.pos))
+        self.velx += total_fx / self.mass * self.TIMESTEP
+        self.vely += total_fy / self.mass * self.TIMESTEP
+        self.x += self.velx * self.TIMESTEP
+        self.y += self.vely * self.TIMESTEP
+        self.orbit.append((self.x, self.y))
 
 
 pygame.init()
@@ -74,9 +82,13 @@ def main():
     sun.sun = True
 
     mercury = Planet(0.387 * Planet.AU, float(0), 8, DARK_GREY, 3.30 * 10**23)
+    mercury.vely = -47.4 * 1000
     venus = Planet(0.723 * Planet.AU, float(0), 14, WHITE, 4.8685 * 10**24)
+    venus.vely = -35.02 * 1000
     earth = Planet(-1 * Planet.AU, float(0), 16, BLUE, 5.9742 * 10**24)
+    earth.vely = 24.077 * 1000
     mars = Planet(-1.524 * Planet.AU, float(0), 12, RED, 6.39 * 10**23)
+    mars.vely = 24.077 * 1000
     
 
     planets = [sun, mercury, venus, earth, mars]
